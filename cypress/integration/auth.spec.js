5f6d5d6d1775ebf1fb575fbe291d736efada9b3f
Anna <anna.krupnovaa@gmail.com>
2021-10-09 16:20:56 -0700

    first test

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -1,17 +1,15 @@
+import LoginPage from '../pages/login.page';
+import ProfilePage from '../pages/profile.page';
+
 describe('Auth', function () {
-    it('successful login', function () {
-       cy.visit('/user/login');
-       cy.get('#normal_login_email')
-           .type('anna.krupnovaa@gmail.com')
-        cy.get('#normal_login_password')
-            .type('123zxc!!')
-        cy.get('.login-form-button')
-          .click();
-       cy.get('.ant-avatar-square')
-           .should('be.visible');
-
-
-
-
+    beforeEach(() => {
+        LoginPage.open();
     });
+
+    it('successful log in', function () {
+       LoginPage.login('anna.krupnovaa@gmail.com', '123zxc!!');
+       ProfilePage.iconAvatar.should('be.visible');
+    });
+
+
 });
