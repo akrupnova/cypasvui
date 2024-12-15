5fde8c347e6fefd6230e7f03286ef655073f0c70
akrupnova <75409569+akrupnova@users.noreply.github.com>
2024-12-14 16:04:59 -0800

    Update auth.spec.js

--- cypress/integration/auth.spec.js
+++ cypress/integration/auth.spec.js
@@ -6,28 +6,28 @@ describe('Auth', function () {
         LoginPage.open();
     });
 
-    it('C6: successful log in', function () {
+    it('C6 successful log in', function () {
        LoginPage.login('anna.krupnovaa@gmail.com', '123zxc!!');
        ProfilePage.iconAvatar.should('be.visible');
     });
 
-    it('C7: wrong credentials throws error', function () {
+    it('C7 wrong credentials throws error', function () {
         LoginPage.login('wrong@mail.com', 'wrong');
         LoginPage.notification.should('contain.text', 'Auth failed');
     });
 
-    it('C8: email validation error', function () {
+    it('C8 email validation error', function () {
         LoginPage.inputEmail.type('wrong');
         LoginPage.emailValidation.should('contain.text', '\'email\' is not a valid email');
     });
 
-    it('C9: email required validation', function ()  {
+    it('C9 email required validation', function ()  {
         LoginPage.inputEmail.type('wrong@wrong.com');
         LoginPage.inputEmail.smartClear();
         LoginPage.emailValidation.should('have.text', 'Required');
     });
 
-    it('C10: password required validation', function ()  {
+    it('C10 password required validation', function ()  {
         LoginPage.inputPassword.type('wrong');
         LoginPage.inputPassword.clear();
         LoginPage.passwordValidation.should('have.text', 'Required');
